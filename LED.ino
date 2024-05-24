df6d08ebdbee59afe4431a43b01a55f680af0fa0
Muhammad Ahmad Samdani <167784209+MuhammadAhmadSamdani@users.noreply.github.com>
2024-05-24 21:58:42 +0500

    LED.ino

--- LED.ino
+++ LED.ino
@@ -1,62 +1,26 @@
-int greenLed = 6;
-int whiteLed = 7;
-int IrSensor = 5;
-int Trepin = 3;
-int Echopin = 2;
-int buzzer = 11;
 
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(greenLed, OUTPUT);
-  pinMode(whiteLed,OUTPUT);
-  pinMode(IrSensor,INPUT);
-  pinMode(buzzer,OUTPUT);
-  pinMode(Trepin,OUTPUT);
-  pinMode(Echopin,INPUT);
-  
-  
-} 
+// LED 
+int RedLed = 3;
+int WhiteLed = 4;
+int GreenLed = 7;
+int YellowLed = 6;
 
-void loop()
-{
-  long duration=5,distance;
-  int value = digitalRead(IrSensor);
-  Serial.println(value);
-  // digitalWrite(greenLed,OUTPUT);
-  digitalWrite(Trepin,LOW);
-  digitalWrite(Trepin,HIGH);
-  delay(1000);
-  digitalWrite(Trepin,LOW);
-  duration = pulseIn(Echopin, HIGH);
-  digitalWrite(Trepin,LOW);
-  distance = duration * 0.034 / 2; 
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-  delay(1000);
-  if(distance<5)
-  {
-    // digitalWrite(buzzer,HIGH);
-    digitalWrite(whiteLed,HIGH);
-  }
-  else if(distance>=5)
-  {
-    digitalWrite(whiteLed,HIGH);
-  }
-   if(value == 1 )
-  {
-   delay(2000);
-    // digitalWrite(buzzer,HIGH);
-    digitalWrite(greenLed,HIGH);
-    digitalWrite(whiteLed,HIGH);
-    delay(1000);
-  }
-  else
-  {
-    delay(1000);
-    digitalWrite(whiteLed,HIGH);
-    delay(1000);
-  }
-  digitalWrite(whiteLed,OUTPUT);
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(RedLed,OUTPUT);
+  pinMode(WhiteLed,OUTPUT);
+  pinMode(GreenLed,OUTPUT);
+  pinMode(YellowLed,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(RedLed, HIGH);
+  delay(2000);
+  digitalWrite(WhiteLed, HIGH);
+  delay(2000);
+  digitalWrite(GreenLed, HIGH);
+  delay(2000);
+  digitalWrite(YellowLed, HIGH);
+  delay(2000);
 }
